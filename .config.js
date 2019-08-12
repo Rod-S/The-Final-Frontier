68ce4324843da19b185f65abf6eb678684950e8f
Rod-S <33836692+Rod-S@users.noreply.github.com>
2019-08-11 22:08:08 -0500

    -NASA PotD API call correctly loading into HTML -added express module for routing (incomplete) -created static folder for static files (incomplete) -directory/file organization

---
+++ .config.js
@@ -0,0 +1,3 @@
+var apiKey = 'PgRhox3uHogMFEF8zpp5zkWiWWEGP0qlMU90vRrH';
+
+module.exports.apiKey = apiKey;
---
+++ routes/index.js
@@ -0,0 +1,47 @@
+//require necessary dependencies
+const express = require('express');
+const app = express();
+
+//setup static route to serve static files in public folder
+app.use('/static', express.static('public'));
+
+//set view engine to read pug files in view folder
+app.set('view engine', 'html');
+
+//route to home webpage, rendering index.pug
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+//route to about page, rendering about.pug
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/')
+
+//setup error middleware, passing in new error object
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  res.locals = data.projects;
+  next(err);
+});
+
+//render status error page rendering error.pug
+app.use(( err, req, res, next ) => {
+  res.locals.error = err;
+  //display specific error statuses depending on code®
+  if (err.status >= 100 && err.status < 600)
+    res.status(err.status);
+  else
+    res.status(500);
+  console.log('There was an error while loading the page');
+  res.render('error');
+});
+
+//start server on localhost port 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
