8cd76404cbf789afd4d0d88797ddd3d98a33c1db
Rod-S <33836692+Rod-S@users.noreply.github.com>
2019-08-15 14:00:10 -0500

    -modified files to allow routes directory -Added Mongoose and mongoDB connection in app.js -created MongoDB models directory/file

---
+++ app.js
@@ -0,0 +1,80 @@
+//require necessary dependencies
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const jsonParser = require('body-parser').json;
+const createError = require('http-errors');
+const mongoose = require('mongoose');
+const routes = require('./routes/index');
+
+const app = express();
+
+//setup static route to serve static files in public folder
+app.use('/static', express.static('public'));
+
+// view engine setup
+app.set('view engine', 'pug');
+
+// mongodb connection
+mongoose.connect("mongodb://localhost:27017/course-api");
+var db = mongoose.connection;
+// display message if mongodb connection error
+db.on('error', console.error.bind(console, 'connection error'));
+// open mongo connection, display message on mongodb success
+db.once("open", function(){
+	console.log("db connection successful");
+});
+
+// morgan gives us http request logging
+app.use(morgan('dev'));
+
+// parse incoming requests
+app.use(jsonParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+module.exports = app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+//start server on localhost port 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,15 @@
-//require necessary dependencies
+//import modules
 const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const createError = require('http-errors');
+const router = express.Router();
 
-const app = express();
-
-//setup static route to serve static files in public folder
-app.use('/static', express.static('public'));
-
-// view engine setup
-app.set('view engine', 'pug');
-
-//route to home webpage, rendering index.pug
-app.get('/', (req, res) => {
+/* GET home page. */
+router.get('/', (req, res, next) => {
   res.render('index');
 });
 
 //route to about page, rendering about.pug
-app.get('/about', (req, res) => {
+router.get('/about', (req, res) => {
   res.render('about');
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handlers
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-//start server on localhost port 3000
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+module.exports = router;
